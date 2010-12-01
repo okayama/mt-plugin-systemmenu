aaabf3852788c065e4438af02d340e6c8a84d05d
okayama <kenmin.okayama@gmail.com>
2010-12-01 18:56:02 +0900

    Support that select display menu from plugin settings.

---
+++ plugins/SystemMenu/lib/MT/SystemMenu/L10N/ja.pm
@@ -0,0 +1,11 @@
+package MT::SystemMenu::L10N::ja;
+
+use strict;
+use base qw/ MT::SystemMenu::L10N MT::L10N MT::Plugin::L10N /;
+use vars qw( %Lexicon );
+
+our %Lexicon = (
+    '_PLUGIN_DESCRIPTION' => 'ナビゲーションメニューにシステムメニューへのリンクを追加します。',
+);
+
+1;
